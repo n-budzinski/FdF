e6f5befe1bccb24fa724958833b6f4853bff0095
Norbert Budzinski <nbudzins@c2r8s1.42warsaw.pl>
2024-06-09 00:36:23 +0200

    Drawing lines between points, fixed the flickering issue

--- fdf.c
+++ fdf.c
@@ -10,8 +10,8 @@
 #define KEYESC  0xff1b //ESCAPE
 #define KEYSPC  0x0020 //SPACE
 
-#define SCREEN_WIDTH 1920
-#define SCREEN_HEIGHT 1080
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
 //gcc -L./minilibx-linux fdf.c -lmlx -lXext -lX11 -lm
 
@@ -24,9 +24,14 @@ typedef struct s_point{
 typedef struct s_params {
     void *mlx;
     void *window;
+    int window_size[2];
+    int map_dimensions[2];
     int mouse_down;
     int last_x;
     int last_y;
+    int bits_per_color;
+    int line_size;
+    int endianess;
     float zoom;
     float x_rot;
     float y_rot;
@@ -34,8 +39,10 @@ typedef struct s_params {
     float tile_width;
     float tile_height;
     float scr_ratio;
-    int map[20][20];
+    char *data;
+    int map[21][21];
     t_point **screenmap;
+    int screen_buffer[SCREEN_WIDTH][SCREEN_HEIGHT];
 }   t_params;
 
 
@@ -220,10 +227,17 @@ void drawLine(int x1, int y1, int x2, int y2, t_params *params) {
     int sy = (y1 < y2) ? 1 : -1;
     int err = dx - dy;
     int e2;
+    int color = mlx_get_color_value(params->mlx, 0xFFFFFF);
 
     while (1) {
-        mlx_pixel_put(params->mlx, params->window, x1, y1, 0xFFFFFF); // Draw the pixel
-        if (x1 == x2 && y1 == y2) break; // Check if the end point is reached
+        if (x1 >= 0 && x1 < SCREEN_WIDTH && y1 >= 0 && y1 < SCREEN_HEIGHT) {
+            int index = x1 * params->bits_per_color / 8 + y1 * params->line_size;
+            params->data[index] = (color & 0xFF);
+            params->data[index + 1] = (color >> 8) & 0xFF;
+            params->data[index + 2] = (color >> 16) & 0xFF;
+            params->data[index + 3] = (color >> 24) & 0xFF;
+        }
+        if (x1 == x2 && y1 == y2) break;
 
         e2 = 2 * err;
         if (e2 > -dy) {
@@ -257,16 +271,19 @@ t_point *calculateScreenPosition(int x, int y, t_params *params)
     // float vec[2];
     multiply3DVector(rotFinal, point, transformed_point);
     // toIsometric(transformed_point, vec);
-    t_point *p = params->screenmap[y * 20 + x];
+    t_point *p = params->screenmap[y * 21 + x];
     p->x = (int)((transformed_point[0] * (params->tile_width * params->zoom) + SCREEN_WIDTH / 2));
     p->y = (int)((transformed_point[1] * (params->tile_height * params->zoom) + SCREEN_HEIGHT / 2));
     p->height = params->map[x][y];
 }
 
+#include <stdio.h>
 int    cb_loop(t_params *params)
 {
     int m_x;
     int m_y;
+    int i;
+    int j;
     mlx_mouse_get_pos(params->mlx, params->window, &m_x, &m_y);
     if (params->mouse_down)
     {
@@ -280,36 +297,53 @@ int    cb_loop(t_params *params)
             m_y = SCREEN_HEIGHT / 2;
         else if (m_y <= SCREEN_HEIGHT / 2)
             m_y = SCREEN_HEIGHT / 2;
-        params->z_rot += (m_x - params->last_x) * 0.1;
-        params->x_rot += (m_y - params->last_y) * 0.1;
+        params->z_rot += (m_x - params->last_x) * 0.05;
+        params->x_rot += (m_y - params->last_y) * 0.05;
         mlx_mouse_move(params->mlx, params->window, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
     }
     else
     {
         params->last_x = m_x;
         params->last_y = m_y;
-        params->z_rot += 0.0005;
+        params->z_rot += 0.005;
     }
-    for (int y = 0; y < 20; y++) {
-        for (int x = 0; x < 20; x++)
+    for (int y = 0; y < 21; y++) {
+        for (int x = 0; x < 21; x++)
             calculateScreenPosition(x, y, params);
     }
-    int i = 0;
+    void *image = mlx_new_image(params->mlx, SCREEN_WIDTH, SCREEN_WIDTH);
+    char *data = mlx_get_data_addr(image, &params->bits_per_color, &params->line_size, &params->endianess);
+    params->data = data;
     t_point *p;
-    mlx_clear_window(params->mlx, params->window);
+    i = 0;
     while (params->screenmap[i] != NULL) {
         p = params->screenmap[i];
-        if (0 != (i + 1) % 20) { // Draw horizontal line
+        if (0 != (i + 1) % 21) {
             drawLine(p->x, p->y, params->screenmap[i + 1]->x, params->screenmap[i + 1]->y, params);
         }
-        if (i < 20 * (20 - 1)) { // Draw vertical line
-            drawLine(p->x, p->y, params->screenmap[i + 20]->x, params->screenmap[i + 20]->y, params);
+        if (i < 21 * (21 - 1)) {
+            drawLine(p->x, p->y, params->screenmap[i + 21]->x, params->screenmap[i + 21]->y, params);
         }
-        // Uncomment if you want to plot the point as well
-        // mlx_pixel_put(params->mlx, params->window, p->x, p->y, 0xFFFFFF);
         i++;
     }
-    return (0);
+    mlx_clear_window(params->mlx, params->window);
+    mlx_put_image_to_window(params->mlx, params->window, image, 0, 0);
+    mlx_destroy_image(params->mlx, image);
+    i = 0;
+    while (i < SCREEN_WIDTH)
+    {
+        j = 0;
+        while (j < SCREEN_HEIGHT)
+        {
+            if (params->screen_buffer[i][j])
+            {
+                mlx_pixel_put(params->mlx, params->window, i, j, params->screen_buffer[i][j]);
+                params->screen_buffer[i][j] = 0;
+            }
+            j++;
+        }
+        i++;
+    }
 }
 
 
@@ -320,12 +354,16 @@ int main()
     void *window;
     t_params *params;
     int loop_val;
+    int i;
+    int j;
 
     mlx = mlx_init();
     window = mlx_new_window(mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "nbudzins's FdF");
     params = malloc(sizeof(t_params));
     params->mlx = mlx;
     params->window = window;
+    params->map_dimensions[0] = 21;
+    params->map_dimensions[1] = 21;
     params->mouse_down = 0;
     params->last_x = 0;
     params->x_rot = 90;
@@ -335,22 +373,36 @@ int main()
     params->tile_width = 30;
     params->tile_height = 30;
     params->scr_ratio = SCREEN_WIDTH / SCREEN_HEIGHT;
-    // for (int i = 0; i < 20; ++i) {
-    //     for (int j = 0; j < 20; ++j)
-    //         params->map[i][j] = 0;
-    // }
-    for (int i = 0; i < 20; ++i) {
-        for (int j = 0; j < 20; ++j) {
-            int dist_to_center = fmax(abs(i - 20 / 2), abs(j - 20 / 2));
-            params->map[i][j] = fmax(0, 10 - dist_to_center + 1);
+    
+    i = 0;
+    while (i < params->window_size[1])
+    {
+        j = 0;
+        while (j < params->window_size[0])
+        {
+            params->screen_buffer[i][j] = 0;
+            j++;
         }
+        i++;
+    }
+    i = 0;
+    while (i < params->map_dimensions[0])
+    {
+        j = 0;
+        while (j < params->map_dimensions[1])
+        {
+            int dist_to_center = fmax(abs(i - 21 / 2), abs(j - 21 / 2));
+            params->map[i][j] = fmax(0, 10 - dist_to_center + 1);
+            j++;
+        }
+        i++;
     }
     //POINT ARRAY INIT//////////////////////////////
     t_point **ptr;
-    ptr = malloc(sizeof(t_point *) * 20 * 20 + 1);
-    ptr[sizeof(t_point *) * 20 * 20] = NULL;
-    int i = 0;
-    while (i < 20 * 20)
+    ptr = malloc(sizeof(t_point *) * 21 * 21 + 1);
+    ptr[sizeof(t_point *) * 21 * 21] = NULL;
+    i = 0;
+    while (i < 21 * 21)
         ptr[i++] = malloc(sizeof(t_point));
     params->screenmap = ptr;
     /////////////////////////////////////////////////
